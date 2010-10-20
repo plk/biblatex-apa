9d94874c14dcfe58030d66df9e7acbd3dd388eec
Philip Kime <Philip@kime.org.uk>
2010-10-20 12:32:23 +0200

    New EPRINT code

--- tp.pl
+++ tp.pl
@@ -1,6 +1,21 @@
 #!/opt/local/bin/perl
 
-use Test::More tests => 1;
-use Test::PDF;
+use Test::More;
+use Test::Deep;
+use CAM::PDF;
 
-cmp_pdf('reference.pdf', 'biblatex-apa-test.pdf', 'PDFs are identical');
+cmp_pdf('biblatex-apa-test.pdf', 'reference.pdf');
+done_testing;
+
+
+sub cmp_pdf ($$) {
+  my ($got, $expected) = @_;
+  $got = CAM::PDF->new($got) or die "Could not create CAM::PDF instance with : $got";
+  $expected = CAM::PDF->new($expected) or die "Could not create CAM::PDF instance with : $expected";
+
+  foreach my $page_num (1 .. $expected->numPages()) {
+    my $tree1 = $got->getPageContentTree($page_num, "verbose");
+    my $tree2 = $expected->getPageContentTree($page_num, "verbose");
+    cmp_deeply($tree1->{blocks}, $tree2->{blocks}, "Page $page_num");
+  }
+}
