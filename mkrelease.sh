f96143288524aeeadd53b49acdb7688313576e5d
Philip Kime <Philip@kime.org.uk>
2014-05-29 16:30:29 +0200

    Moved to dir structure as per #5

---
+++ mkrelease.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir biblatex-apa
+find -E . -regex '.*\.(bbx|cbx|dbx|lbx|pdf|tex|bib)' | xargs -J % cp % biblatex-apa/
+\rm biblatex-apa/reference.pdf
+tar zcf biblatex-apa.tgz biblatex-apa
+\rm -rf biblatex-apa
