4c21051048d05c05482ad15ffc7a5fd1a51df30c
Philip Kime <Philip@kime.org.uk>
2015-01-23 09:48:53 +0100

    Addedd README to package build

--- mkrelease.sh
+++ mkrelease.sh
@@ -2,6 +2,7 @@
 
 mkdir biblatex-apa
 find -E . -regex '.*\.(bbx|cbx|dbx|lbx|pdf|tex|bib)' | xargs -J % cp % biblatex-apa/
+cp README biblatex-apa/
 \rm biblatex-apa/reference.pdf
 tar zcf biblatex-apa.tgz biblatex-apa
 \rm -rf biblatex-apa
