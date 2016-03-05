e16466dbd80d8e628b4459c53f6e555e6051b74e
Philip Kime <Philip@kime.org.uk>
2016-03-05 11:26:45 +0100

    Better install script

--- install.sh
+++ install.sh
@@ -2,5 +2,15 @@
 
 find -E . -regex '.*\.(bbx|cbx|dbx|lbx)' | xargs -J % cp % ~/texmf/tex/latex/biblatex-apa/
 cp bibtex/bib/*.bib ~/texmf/bibtex/bib/biblatex-apa/
-cp doc/biblatex-apa* ~/texmf/doc/latex/biblatex-apa/
+cd doc
+\rm *.{aux,bbl,bcf,blg,log,run.xml,toc,out,lot,synctex} 2>/dev/null
+lualatex -interaction=batchmode biblatex-apa.tex
+lualatex -interaction=batchmode biblatex-apa.tex
+lualatex -interaction=batchmode biblatex-apa.tex
+lualatex -interaction=batchmode biblatex-apa-test.tex
+biber --quiet biblatex-apa-test
+lualatex -interaction=batchmode biblatex-apa-test.tex
+lualatex -interaction=batchmode biblatex-apa-test.tex
+cd ..
+cp doc/biblatex-apa*.pdf ~/texmf/doc/latex/biblatex-apa/
 
