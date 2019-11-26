11412e5f78c0cbea49bd82dc75b77d1c672eda39
Philip Kime <Philip@kime.org.uk>
2019-11-26 22:17:46 +0100

    Updated apa6 scrupts

--- install.sh
+++ install.sh
@@ -7,13 +7,13 @@ if [[ "$1" == "all" ]]
 then
   cd doc
   \rm *.{aux,bbl,bcf,blg,log,run.xml,toc,out,lot,synctex} 2>/dev/null
-  lualatex -interaction=batchmode biblatex-apa.tex
-  lualatex -interaction=batchmode biblatex-apa.tex
-  lualatex -interaction=batchmode biblatex-apa.tex
-  lualatex -interaction=batchmode biblatex-apa-test.tex
+  lualatex -interaction=batchmode biblatex-apa6.tex
+  lualatex -interaction=batchmode biblatex-apa6.tex
+  lualatex -interaction=batchmode biblatex-apa6.tex
+  lualatex -interaction=batchmode biblatex-apa6-test.tex
   biber --quiet biblatex-apa-test
-  lualatex -interaction=batchmode biblatex-apa-test.tex
-  lualatex -interaction=batchmode biblatex-apa-test.tex
+  lualatex -interaction=batchmode biblatex-apa6-test.tex
+  lualatex -interaction=batchmode biblatex-apa6-test.tex
   \rm *.{aux,bbl,bcf,blg,log,run.xml,toc,out,lot,synctex} 2>/dev/null
   cd ..
   cp doc/biblatex-apa*.pdf ~/texmf/doc/latex/biblatex-apa/
--- mkrelease.sh
+++ mkrelease.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-mkdir biblatex-apa
-find -E . -regex '.*\.(bbx|cbx|dbx|lbx|pdf|tex|bib)' | xargs -J % cp % biblatex-apa/
-cp README biblatex-apa/
-\rm biblatex-apa/reference.pdf
-tar zcf biblatex-apa.tgz biblatex-apa
-\rm -rf biblatex-apa
+mkdir biblatex-apa6
+find -E . -regex '.*\.(bbx|cbx|dbx|lbx|pdf|tex|bib)' | xargs -J % cp % biblatex-apa6/
+cp README biblatex-apa6/
+\rm biblatex-apa6/reference.pdf
+tar zcf biblatex-apa6.tgz biblatex-apa6
+\rm -rf biblatex-apa6
