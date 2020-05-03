b6ab42d29cab369c331de9f1ea33868ae6a1d45d
Philip Kime <Philip@kime.org.uk>
2020-05-03 13:48:09 +0200

    Don't package apa6 docs, if present

--- mkrelease.sh
+++ mkrelease.sh
@@ -4,5 +4,9 @@ mkdir biblatex-apa
 find -E . -regex '.*\.(bbx|cbx|dbx|lbx|pdf|tex|bib)' | xargs -J % cp % biblatex-apa/
 cp README biblatex-apa/
 \rm biblatex-apa/reference.pdf
+# Ignore apa6 docs, if present
+\rm -f biblatex-apa6/biblatex-apa6.pdf
+\rm -f biblatex-apa6/biblatex-apa6-test.pdf
+
 tar zcf biblatex-apa.tgz biblatex-apa
 \rm -rf biblatex-apa
